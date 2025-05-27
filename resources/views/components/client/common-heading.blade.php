b26a6dfa277bfad84bd796ebfa48016a37e43595
aizubair21 <amimulit21@gmail.com>
2025-05-27 21:47:38 +0600

    Simple test for cPanel

--- resources/views/components/client/common-heading.blade.php
+++ resources/views/components/client/common-heading.blade.php
@@ -1,7 +1,7 @@
 <div>
     <div class="heading_container heading_center my-3">
         <h2 class="text-xl">
-            ERhi <span>Bazar</span>
+            Eruhi <span>Market</span>
         </h2>
     </div>
 </div>
