fef3041347b18ce3a3ed42e0f7337c3aa53e08c0
aizubair21 <amimulit21@gmail.com>
2025-09-01 14:40:39 +0600

    resovle homepage category display mis-design

--- resources/views/livewire/welcome.blade.php
+++ resources/views/livewire/welcome.blade.php
@@ -1,21 +1,24 @@
 <div>
-    {{-- <link rel="stylesheet" type="text/css" href="{{asset('assets/css/bootstrap.min.css')}}" /> --}}
+    {{--
+    <link rel="stylesheet" type="text/css" href="{{asset('assets/css/bootstrap.min.css')}}" /> --}}
 
     <style>
         /* @media only screen and (max-width: 600px) { */
         @media (min-width: 767px) {
             .detail-box h1 {
-                font-size: 3rem!important;
+                font-size: 3rem !important;
                 margin-bottom: 0px;
             }
         }
+
         @media (min-width: 1199px) {
             .detail-box h1 {
-                font-size: 4rem!important;
+                font-size: 4rem !important;
                 margin-bottom: 0px;
             }
         }
-        @media (max-width: 570px){
+
+        @media (max-width: 570px) {
 
             .slider_bg_box img {
                 width: 100%;
@@ -26,16 +29,19 @@
                 object-position: top right; */
                 aspect-ratio: 16 / 9;
             }
+
             .detail-box h1 {
-                font-size: 4rem!important;
+                font-size: 4rem !important;
                 margin-bottom: 0px;
             }
         }
+
         @media (max-width: 767px) {
 
             .slider {
                 /* height:200px!important; */
-                }
+            }
+
             /* .slider_bg_box img {
                 width: 100%;
                 height: 100%;
@@ -77,79 +83,82 @@
 
 
     <x-dashboard.container>
-        {{-- categories  --}}
+        {{-- categories --}}
         @if (count($categories))
         <div class="pt-4">
             Categories
         </div>
-        
-        
-        <div x-loading.disabled x-transition class="pb-4" style="display: grid; grid-template-columns:repeat(auto-fit, 100px); grid-gap:10px">
-                @foreach ($categories as $item)
-                    @if ($item->slug != 'default-category')         
-                        <div class="relative text-center rounded-md " style="backdrop-filter:blur(3px); background-color:#fff; height:80px">
-                            <a href="{{ route('category.products', ['cat' => $item->slug]) }}" style="height: 100px;" wire:navigate >
-                                <img src="{{asset('storage/'.$item->image)}}" class="w-full h-full" alt="">
-                                <div class="absolute bottom-0 shadow w-full text-center" style="background-color:
-                                #f6f6f69c; backdrop-filter:blur(6px)"> 
-                                    {{ Str::limit($item->name, 9, '...') }}
-                                </div>
-                            </a>
-                        </div>
-                    @endif
-                @endforeach
+
+
+        <div x-loading.disabled x-transition class="pb-4"
+            style="display: grid; grid-template-columns:repeat(auto-fit, 100px); grid-gap:10px">
+            @foreach ($categories as $item)
+            @if ($item->slug != 'default-category')
+            <div class="relative text-center rounded-md "
+                style="backdrop-filter:blur(3px); background-color:#fff; height:100px">
+                <a href="{{ route('category.products', ['cat' => $item->slug]) }}" style="height: 100px;" wire:navigate>
+                    <img src="{{asset('storage/'.$item->image)}}" class="w-full h-full" alt="">
+                    <div class="absolute bottom-0 shadow w-full text-center" style="background-color:
+                                        #f6f6f69c; backdrop-filter:blur(6px)">
+                        {{ Str::limit($item->name, 9, '...') }}
+                    </div>ss
+                </a>
             </div>
             @endif
-        
-            <div x-init="$wire.getProducts">
-                <div class="py-4 flex px-2 justify-between items-center">
-                    <div class="text-xl font-bold">
-                        New Arrival
-                    </div>
-                    
-                    <div class="text-center">
-                        <x-nav-link href="{{route('products.index')}}" class="px-3 py-2 rounded ">
-                            View All
-                        </x-nav-link>
-                    </div>
-                </div>
-                <div class="product_section pt-4" x-loading.disabled x-transition>
-                    {{-- @includeIf('components.client.common-heading') --}}
-                    {{-- <x-client.products-loop :$products /> --}}
-                    @if (count($products))     
-                        <div class="" style="display: grid; justify-content:start; grid-template-columns: repeat(auto-fill, 160px); grid-gap:10px">
-                            @foreach($products as $product)
-                                <x-client.product-cart :$product :key="$product->id" />
-                            @endforeach    
-                        </div>
-                    @endif
-            
-                </div>
-           
-               @livewire('pages.topSales')
-                
-                @livewire('pages.RecomendedProducts')
-            </div>
-
+            @endforeach
         </div>
-    
+        @endif
 
-         
-        {{-- <div class="py-4 flex px-2 justify-between items-center">
-            <div class="text-xl font-bold">
-                Top Sell
+        <div x-init="$wire.getProducts">
+            <div class="py-4 flex px-2 justify-between items-center">
+                <div class="text-xl font-bold">
+                    New Arrival
+                </div>
+
+                <div class="text-center">
+                    <x-nav-link href="{{route('products.index')}}" class="px-3 py-2 rounded ">
+                        View All
+                    </x-nav-link>
+                </div>
             </div>
-            
-            
-        </div> --}}
-        {{-- @livewire('reseller.resel.categories') --}}
-    </x-dashboard.container>
+            <div class="product_section pt-4" x-loading.disabled x-transition>
+                {{-- @includeIf('components.client.common-heading') --}}
+                {{--
+                <x-client.products-loop :$products /> --}}
+                @if (count($products))
+                <div class=""
+                    style="display: grid; justify-content:start; grid-template-columns: repeat(auto-fill, 160px); grid-gap:10px">
+                    @foreach($products as $product)
+                    <x-client.product-cart :$product :key="$product->id" />
+                    @endforeach
+                </div>
+                @endif
 
-    @script
-    
-        <script>
-      
-            const slides = document.querySelectorAll(".slide");
+            </div>
+
+            @livewire('pages.topSales')
+
+            @livewire('pages.RecomendedProducts')
+        </div>
+
+</div>
+
+
+
+{{-- <div class="py-4 flex px-2 justify-between items-center">
+    <div class="text-xl font-bold">
+        Top Sell
+    </div>
+
+
+</div> --}}
+{{-- @livewire('reseller.resel.categories') --}}
+</x-dashboard.container>
+
+@script
+
+<script>
+    const slides = document.querySelectorAll(".slide");
             const prevBtn = document.querySelector(".prev");
             const nextBtn = document.querySelector(".next");
 
@@ -170,7 +179,7 @@
                 showSlide(current);
             });
     
-        </script>
-    @endscript
+</script>
+@endscript
 
 </div>
