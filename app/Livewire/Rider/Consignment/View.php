0ad981c2dac508f5b2e7bb9ec6e9e415f5602884
aizubair21 <amimulit21@gmail.com>
2025-09-15 14:28:50 +0600

    add rider consignment view at rider consignment

---
+++ app/Livewire/Rider/Consignment/View.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Livewire\Rider\Consignment;
+
+use App\Models\CartOrder;
+use App\Models\cod;
+use App\Models\Order;
+use App\Models\User;
+use Livewire\Component;
+use Livewire\Attributes\Layout;
+use Livewire\Attributes\Url;
+
+#[Layout('layouts.app')]
+class View extends Component
+{
+
+    #[Url]
+    public $id;
+
+
+    public function render()
+    {
+        $dta = cod::findOrFail($this->id);
+        return view('livewire.rider.consignment.view', [
+            'cod' => $dta,
+            'order' => $dta->order,
+            'co' => CartOrder::where('order_id', $dta->order_id)->get(),
+            'seller' => User::findOrFail($dta->seller_id),
+            'user' => User::findOrFail($dta->user_id)
+        ]);
+    }
+}
