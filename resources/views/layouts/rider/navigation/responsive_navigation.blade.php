919eb5a3c7e9ed4bf373e05b6abf23116f621840
aizubair21 <amimulit21@gmail.com>
2025-09-15 16:41:42 +0600

    add rider-info navigation and page to rider dashboard to see quick information about herself

--- resources/views/layouts/rider/navigation/responsive_navigation.blade.php
+++ resources/views/layouts/rider/navigation/responsive_navigation.blade.php
@@ -1,10 +1,9 @@
-<x-responsive-nav-link href="{{route('my-shop', ['user' => auth()->user()->name])}}"
-    :active="request()->routeIs('my-shop')">
+<x-responsive-nav-link href="{{route('rider.me')}}" :active="request()->routeIs('rider.me')">
     <i class="fas fa-person-biking pr-2 w-6"></i> My Rider
 </x-responsive-nav-link>
 <x-hr />
 
-<x-responsive-nav-link href="{{route('rider.consignment')}}" :active="request()->routeIs('rider.*')">
+<x-responsive-nav-link href="{{route('rider.consignment')}}" :active="request()->routeIs('rider.consignment')">
     <i class="fas fa-truck-fast pr-2 w-6"></i> Consignments
 </x-responsive-nav-link>
 
--- routes/rider.php
+++ routes/rider.php
@@ -1,9 +1,12 @@
 <?php
 
+use App\Http\Middleware\AbleTo;
 use App\Livewire\Rider\Consignment\Index;
 use App\Livewire\Rider\Consignment\View;
+use App\Livewire\Rider\RiderInfo;
 use Illuminate\Support\Facades\Route;
 
-Route::get('my-consignments', Index::class)->name('rider.consignment');
+Route::get('my-consignments', Index::class)->name('rider.consignment')->middleware(AbleTo::class . ':access_rider_dashboard');
 // Route::get('my-consignments', Index::class)->name('rider.consignment');
-Route::get('/consignments/', View::class)->name('rider.consignment.view');
+Route::get('/consignments/', View::class)->name('rider.consignment.view')->middleware(AbleTo::class . ':access_rider_dashboard');
+Route::get('/me', RiderInfo::class)->name('rider.me')->middleware(AbleTo::class . ':access_rider_dashboard');
