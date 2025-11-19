24de840ba7e0459d3850c69c71bf889d8e4f3ac4
aizubair21 <amimulit21@gmail.com>
2025-11-19 23:17:24 +0600

    modify rider migration

--- database/migrations/2025_11_16_015627_add_colum_to_rider_table.php
+++ database/migrations/2025_11_16_015627_add_colum_to_rider_table.php
@@ -12,15 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('riders', function (Blueprint $table) {
-            // 'vehicle_type' => $this->vehicle_type,
-            // 'vehicle_number' => $this->vehicle_number,
-            // 'vehicle_model' => $this->vehicle_model,
-            // 'vehicle_color' => $this->vehicle_color,
             $table->string('city', 155)->nullable();
-            $table->string('vehicle_type', 191)->nullable();
-            $table->string('vehicle_number', 191)->nullable();
-            $table->string('vehicle_model', 191)->nullable();
-            $table->string('vehicle_color', 191)->nullable();
         });
     }
 
@@ -32,10 +24,7 @@ return new class extends Migration
         Schema::table('rider', function (Blueprint $table) {
             $table->dropIfExists(
                 [
-                    'vehicle_type',
-                    'vehicle_number',
-                    'vehicle_model',
-                    'vehicle_color',
+                    'city',
                 ]
             );
         });
