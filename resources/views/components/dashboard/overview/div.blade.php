9679589f5dc24b6596f908143e338dae2f3d0938
aizubair21 <amimulit21@gmail.com>
2025-12-25 08:15:35 +0600

    overview gradient fill

--- resources/views/components/dashboard/overview/div.blade.php
+++ resources/views/components/dashboard/overview/div.blade.php
@@ -1,41 +1,27 @@
-<div class="rounded d-block shadow p-3 relative overflow-hidden"
-    style="background-color: orange; z-index:1; color:white;">
+<div class="rounded d-block border p-3 relative overflow-hidden" style="z-index:1; color:white;">
     <!-- The biggest battle is the war against ignorance. - Mustafa Kemal Atatürk -->
     <style>
         .div_wrapper {
             position: absolute;
-            bottom: -100px;
-            right: -100px;
+            top: 50px;
+            left: -100px;
+            transform: translateY(-50%);
             width: 200px;
             height: 200px;
             border-radius: 50%;
-            background: radial-gradient(rgb(12, 165, 94), transparent);
+            background: radial-gradient(rgba(255, 166, 0, 0.418), transparent);
             /* background: radial-gradient(hsl(22, 97%, 65%), transparent); */
             z-index: -1;
         }
-
-        .div_wrapper::after {
-            content: "";
-            position: absolute;
-            width: 80px;
-            height: 80px;
-            top: 50%;
-            left: 50%;
-            transform: translate(-50%, -50%);
-            border-radius: 50%;
-            background: radial-gradient(green, transparent);
-            /* border: 1px dotted indigo; */
-
-        }
     </style>
-    <div class="text-md mb-3">
-        {{$title ?? "Overview"}}
-    </div>
 
-    <div class="text-end text-2xl">
+    <div class="text-3xl text-gray-800">
         {{$content ?? " 0 / 0"}}
     </div>
 
+    <div class="text-sm mb-1 text-gray-500">
+        {{$title ?? "Overview"}}
+    </div>
     <div class="div_wrapper"></div>
 
 </div>
